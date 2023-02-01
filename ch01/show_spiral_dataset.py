eb7f36cac1377bb31e8ffbaa6f29c679b35014b8
Blueness <w0330t@gmail.com>
2023-02-01 21:48:23 +0800

    添加一个显示数据的玩意（抄的）

---
+++ ch01/show_spiral_dataset.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('.')
+
+from dataset import spiral
+import matplotlib.pyplot as plt
+
+x, t = spiral.load_data()
+# print(x)
+# print(t)
+
+# 绘制数据点
+N = 100
+CLS_NUM = 3
+markers = ['o', 'x', '^']
+for i in range(CLS_NUM):
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+plt.show()
