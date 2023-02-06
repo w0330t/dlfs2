2e6b08c862f50da3c7a1357c55de1f59ec4762bf
Blueness <w0330t@gmail.com>
2023-02-06 13:45:01 +0800

    添加利用单词创建矩阵

--- common/util.py
+++ common/util.py
@@ -5,7 +5,7 @@ def preprocess(text):
     word_to_id = {}
     id_to_word = {}
 
-    words = re.findall(r'\b\w+\b', text)
+    words = re.findall(r'\b\w+\b|[^\w\s]+', text)
     words = [word.lower() for word in words]
 
 
@@ -18,3 +18,30 @@ def preprocess(text):
     corpus = torch.tensor([word_to_id[w] for w in words])
     
     return corpus, word_to_id, id_to_word
+
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''生成共现矩阵
+
+    :param corpus: 语料库（单词ID列表）
+    :param vocab_size:词汇个数
+    :param window_size:窗口大小（当窗口大小为1时，左右各1个单词为上下文）
+    :return: 共现矩阵
+    '''
+    corpus_size = len(corpus)
+    co_matrix = torch.zeros((vocab_size, vocab_size))
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
